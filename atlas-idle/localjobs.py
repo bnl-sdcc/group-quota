f2181344074b17e02f8faa3b52e8c7a76acf30d1
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-08-21 11:53:06 -0400

    Update.

--- atlas-idle/localjobs.py
+++ atlas-idle/localjobs.py
@@ -27,16 +27,16 @@ from libfactory.info import StatusInfo, IndexByKey, AnalyzerFilter, AnalyzerMap,
 
 from gq import config as gqconfig
 
-# Queues to watch, map of PANDA Name -> Condor Group name
-GROUPS = [
-    'group_atlas.prod.xl',
-    'group_atlas.prod.mp',
-    'group_atlas.prod.test',
-    'group_atlas.prod.production',
-    'group_atlas.analysis.short',
-    'group_atlas.analysis.long',
-    'group_atlas.analysis.amc',
-    ]
+# Queues to watch 
+GROUPS = {
+    'group_atlas.prod.xl': 1,
+    'group_atlas.prod.mp': 1,
+    'group_atlas.prod.test': 1,
+    'group_atlas.prod.production': 1,
+    'group_atlas.analysis.short': 1,
+    'group_atlas.analysis.long': 1,
+    'group_atlas.analysis.amc': 1,
+    }
 
 class StripUserMap(AnalyzerMap):
     def map(self, job):
@@ -66,30 +66,38 @@ class AtlasOnlyFilter(AnalyzerFilter):
         matches = False
         try:
             ag = job['accountinggroup']
-            matches = 'group_atlas' in ag
+            v = GROUPS[ag]
+            matches = True
         except:
-            pass
+            matches = False
         return matches
         
     
 def get_jobs():
+    jobdict = {}
+    try:
+        cm_host = gqconfig.condor_cm.split(':')[0]
+        cm_port = gqconfig.condor_cm.split(':')[1]         
     
-    cm_host = gqconfig.condor_cm.split(':')[0]
-    cm_port = gqconfig.condor_cm.split(':')[1]     
-    pool = HTCondorPool(hostname=cm_host, port=cm_port)
-    #sd = HTCondorSchedd()
-    attlist = ['accountinggroup','jobstatus','xcount']
-    cq = pool.condor_q(attribute_l = attlist)
-    si = StatusInfo(cq)
-    idlefilter = IdleOnlyFilter()  
-    stripusermap = StripUserMap()
-    atlasfilter = AtlasOnlyFilter()
-    si = si.filter(idlefilter)
-    si = si.map(stripusermap)
-    si = si.filter(atlasfilter)
-    si = si.indexby(IndexByKey('accountinggroup'))   
-    si = si.process(Count())
-    return si.getraw()
+        pool = HTCondorPool(hostname=cm_host, port=cm_port)
+        #sd = HTCondorSchedd()
+        attlist = ['accountinggroup','jobstatus','xcount']
+        cq = pool.condor_q(attribute_l = attlist)
+        si = StatusInfo(cq)
+        idlefilter = IdleOnlyFilter()  
+        stripusermap = StripUserMap()
+        atlasfilter = AtlasOnlyFilter()
+        si = si.filter(idlefilter)
+        si = si.map(stripusermap)
+        si = si.filter(atlasfilter)
+        si = si.indexby(IndexByKey('accountinggroup'))   
+        si = si.process(Count())
+        jobdict = si.getraw()
+    except:
+        pass
+    
+    return jobdict
+        
 
 
 if __name__ == '__main__':
